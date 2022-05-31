f28a01c586df5f6f4f30fd0a9e79d0e9db10693a
Genhao Li <1355401384@qq.com>
2022-05-31 22:47:14 +0800

    extract comp: NotificationIcon, UserInfoIcon, NavBarForEdit; global css; remove eslint pkg

---
+++ src/components/UserInfoIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <i class="el-icon-user clickable" @click="showUserInfo"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserInfo',
+  methods: {
+    showUserInfo() {
+      // TODO
+      this.$message({
+        message: '点击了用户信息',
+        type: 'success'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
