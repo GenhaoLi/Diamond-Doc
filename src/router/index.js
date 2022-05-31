a2b3de2a07f82e1842ec24c7edebe94e47c0a797
Genhao Li <1355401384@qq.com>
2022-05-31 18:27:35 +0800

    add router routes; add views inside workspace and edit view; adjust el-aside

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,25 @@ const routes = [
     component: HomeView
   },
   {
+    path: '/workspace',
+    name: 'workspace',
+    component: () => import('../views/WorkspaceView.vue'),
+    children: [
+      {
+        path: 'files',
+        name: 'files',
+        component: () => import('../views/FilesView.vue')
+      }
+    ]
+  },
+  {
     path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
 ]
 
 const router = new VueRouter({
